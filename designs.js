33b1208fdc5a90ceaba9527c8d71d421502c6128
Emem Umoh <umohes@yahoo.com>
2018-06-28 02:40:58 +0000

    Initial commit

--- designs.js
+++ designs.js
@@ -1,10 +1,45 @@
-// Select color input
-// Select size input
+$(function () {
+    //Set the grid and current color to #000
+    var theGrid = $("#pixel_canvas");
+    var currentColor = "#000";
 
-// When size is submitted by the user, call makeGrid()
+    // Prevent default submit behavior and call makeGrid function
+    $("#sizePicker").submit(function (e) {
+        e.preventDefault();
+        makeGrid();
+    });
 
-function makeGrid() {
+    //Change the currentColor to the selected one
+    $("#colorPicker").change(function () {
+        currentColor = $(this).val();
+    });
 
-// Your code goes here!
+    //If the user clicks or moves the mouse in the grid
+    //while holding the left mouse button down draw the pixels
+    theGrid.on("mousemove", "td", function (e) {
+        if (e.buttons == 1) {
+            $(this).css("background-color", currentColor); //Draw the td that the user hovers
+        }
+    });
 
-}
+    theGrid.on("click", "td", function (e) {
+        $(this).css("background-color", currentColor); //Draw the td that the user clicks
+    });
+
+    //Build the grid
+    function makeGrid() {
+        //Set the grid dimensions;
+        var gridHeight = $("#input_height").val();
+        var gridWidth = $("#input_width").val();
+        theGrid.empty(); //Empty the grid and start over
+        //Loop -> Create rows
+        for (x = 0; x <= gridHeight - 1; x++) {
+            theGrid.append("<tr>"); //Start Row
+            //Loop -> Create columns for each row
+            for (y = 0; y <= gridWidth - 1; y++) {
+                $("tr:eq(" + x + ")").append("<td></td>");
+            }
+            theGrid.append("</tr>"); //End Row
+        }
+    }
+});
